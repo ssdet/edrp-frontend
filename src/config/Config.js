d28835cf1a5211dbf9bee1ba83fcc17f3dd18fed
puneetbhatt <puneetbhatt2000@gmail.com>
2021-07-03 21:21:27 +0530

    Updated With Criterions

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,2 +1,11 @@
-export const GLOBAL_URL = "http://localhost:8000/api";
+
+const GLOBAL_URL = "";
+
+if(process.env.NODE_ENV === 'development') {
+    GLOBAL_URL = 'http://localhost:8000/api'
+  }
+  
+  if(process.env.NODE_ENV === 'production') {
+    GLOBAL_URL = 'https://api.hnbguiqac.in/api'
+  }
 // https://corsanywhere.herokuapp.com/https://freereddittesting.000webhostapp.com/api/
